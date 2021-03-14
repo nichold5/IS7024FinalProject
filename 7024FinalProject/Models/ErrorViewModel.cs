9d181a5e1ba97038e61b3bd56b71f66d778917ca
Damon <63312122+nichold5@users.noreply.github.com>
2021-03-14 16:17:28 -0400

    Solution framework created

---
+++ 7024FinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _7024FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
